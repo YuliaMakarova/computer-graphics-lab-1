1ad6811f3304f64caa223147f295672af40176a2
YuliaMakarova <juliamakarova2172@gmail.com>
2023-10-12 19:04:13 +0400

    добавление комментариев к коду

--- main.py
+++ main.py
@@ -1,41 +1,61 @@
 import tkinter as tk
 
 
+# Создаем класс CanvasApp, который наследует от tk.Tk и представляет наше приложение
 class CanvasApp(tk.Tk):
     def __init__(self):
+        # Инициализируем родительский класс tk.Tk
         tk.Tk.__init__(self)
+
+        # Создаем холст (Canvas) для рисования
         self.canvas = tk.Canvas(self)
 
-        self.hx = 10
-        self.hy = 10
-        self.x1 = 20
-        self.y1 = 20
-        self.x2 = 300
-        self.y2 = 200
+        # Начальные координаты и размеры прямоугольников
+        self.hx = 10  # Шаг изменения по горизонтали
+        self.hy = 10  # Шаг изменения по вертикали
+        self.x1 = 20  # Левая верхняя координата x
+        self.y1 = 20  # Левая верхняя координата y
+        self.x2 = 300  # Правая нижняя координата x
+        self.y2 = 200  # Правая нижняя координата y
+
+        # Список для хранения идентификаторов созданных прямоугольников
         self.rectangles = []
 
+        # Размещаем холст так, чтобы он заполнил все доступное пространство окна
         self.canvas.pack(fill=tk.BOTH, expand=1)
 
-        self.bind('<Return>', self.draw)
-        self.bind('<Escape>', self.close)
-        self.bind("<KeyPress-d>", self.delete)
+        # Привязываем события клавиш к соответствующим методам
+        self.bind('<Return>', self.draw)  # Нажатие Enter вызывает метод draw
+        self.bind('<Escape>', self.close)  # Нажатие Escape вызывает метод close
+        self.bind('<KeyPress-d>', self.delete)  # Нажатие клавиши "D" вызывает метод delete
 
+    # Метод для рисования прямоугольников
     def draw(self, event):
         if self.x1 < self.x2 and self.y1 < self.y2:
-            self.rectangles.append(self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, outline="black"))
+            # Создаем прямоугольник на холсте
+            rectangle = self.canvas.create_rectangle(self.x1, self.y1, self.x2, self.y2, outline="black")
+            # Добавляем идентификатор прямоугольника в список
+            self.rectangles.append(rectangle)
+            # Изменяем координаты и размер для следующего прямоугольника
             self.x1 += self.hx
             self.y1 += self.hy
             self.x2 -= self.hx
             self.y2 -= self.hy
 
+    # Метод для удаления последнего прямоугольника
     def delete(self, event):
         if len(self.rectangles) != 0:
-            self.canvas.delete(self.rectangles.pop())
+            # Получаем идентификатор последнего прямоугольника из списка
+            last_rectangle = self.rectangles.pop()
+            # Удаляем прямоугольник с холста
+            self.canvas.delete(last_rectangle)
+            # Восстанавливаем предыдущие координаты и размер
             self.x1 -= self.hx
             self.y1 -= self.hy
             self.x2 += self.hx
             self.y2 += self.hy
 
+    # Метод для закрытия приложения
     def close(self, event):
         self.destroy()
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,41 +1,46 @@
 import sys
 import os
-
-# Получите текущий каталог (где находится test_main.py)
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Добавьте каталог lab1 (где находится main.py) в sys.path
-sys.path.append(os.path.join(current_dir, '..'))
-
 import unittest
 import tkinter as tk
 from main import CanvasApp
 
+# Получаем путь к текущему каталогу скрипта
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Добавляем путь к родительскому каталогу
+sys.path.append(os.path.join(current_dir, '..'))
 
+# Создаем класс TestCanvasApp, который будет содержать тесты
 class TestCanvasApp(unittest.TestCase):
     def setUp(self):
+        # Создаем корневое окно Tkinter
         self.root = tk.Tk()
+        # Создаем экземпляр CanvasApp
         self.app = CanvasApp()
+        # Получаем доступ к холсту приложения
         self.canvas = self.app.canvas
 
     def tearDown(self):
+        # Закрываем корневое окно после завершения каждого теста
         self.root.destroy()
 
     def test_draw(self):
-        initial_rectangles = len(self.canvas.find_all())
-        event = tk.Event()
-        self.app.draw(event)
-        updated_rectangles = len(self.canvas.find_all())
-        self.assertEqual(updated_rectangles, initial_rectangles + 1)
+        # Тест для проверки метода draw
+        initial_rectangles = len(self.canvas.find_all())  # Получаем начальное количество прямоугольников
+        event = tk.Event()  # Создаем событие (для имитации события клавиши Enter)
+        self.app.draw(event)  # Вызываем метод draw
+        updated_rectangles = len(self.canvas.find_all())  # Получаем обновленное количество прямоугольников
+        self.assertEqual(updated_rectangles, initial_rectangles + 1)  # Сравниваем с ожидаемым результатом
 
     def test_delete(self):
-        event = tk.Event()
-        self.app.draw(event)
-        initial_rectangles = len(self.canvas.find_all())
-        event = tk.Event()
-        self.app.delete(event)
-        updated_rectangles = len(self.canvas.find_all())
-        self.assertEqual(updated_rectangles, initial_rectangles - 1)
+        # Тест для проверки метода delete
+        event = tk.Event()  # Создаем событие
+        self.app.draw(event)  # Рисуем прямоугольник
+        initial_rectangles = len(self.canvas.find_all())  # Получаем начальное количество прямоугольников
+        event = tk.Event()  # Создаем событие (для имитации события клавиши "d")
+        self.app.delete(event)  # Вызываем метод delete
+        updated_rectangles = len(self.canvas.find_all())  # Получаем обновленное количество прямоугольников
+        self.assertEqual(updated_rectangles, initial_rectangles - 1)  # Сравниваем с ожидаемым результатом
+
 
 
 if __name__ == '__main__':
