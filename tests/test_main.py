aa846890e31a78d2da98e4fd7ae7c4e7fcabbc2a
YuliaMakarova <juliamakarova2172@gmail.com>
2023-10-12 20:17:15 +0400

    добавление комментариев к коду

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,39 +9,48 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # Добавляем путь к родительскому каталогу
 sys.path.append(os.path.join(current_dir, '..'))
 
-# Создаем класс TestCanvasApp, который будет содержать тесты
 class TestCanvasApp(unittest.TestCase):
+    """
+    Класс TestCanvasApp содержит юнит-тесты для проверки функциональности класса CanvasApp.
+
+    Атрибуты:
+    - root: корневое окно Tkinter
+    - app: экземпляр CanvasApp, объект, который будет тестироваться
+    - canvas: холст приложения для тестирования
+
+    Методы:
+    - setUp: выполняется перед каждым тестом, создает корневое окно и экземпляр CanvasApp
+    - tearDown: выполняется после каждого теста, закрывает корневое окно
+    - test_draw: тест для проверки метода draw
+    - test_delete: тест для проверки метода delete
+
+    Параметры методов:
+    - event: объект события, используется для имитации событий клавиш
+    """
+
     def setUp(self):
-        # Создаем корневое окно Tkinter
         self.root = tk.Tk()
-        # Создаем экземпляр CanvasApp
         self.app = CanvasApp()
-        # Получаем доступ к холсту приложения
         self.canvas = self.app.canvas
 
     def tearDown(self):
-        # Закрываем корневое окно после завершения каждого теста
         self.root.destroy()
 
     def test_draw(self):
-        # Тест для проверки метода draw
-        initial_rectangles = len(self.canvas.find_all())  # Получаем начальное количество прямоугольников
-        event = tk.Event()  # Создаем событие (для имитации события клавиши Enter)
-        self.app.draw(event)  # Вызываем метод draw
-        updated_rectangles = len(self.canvas.find_all())  # Получаем обновленное количество прямоугольников
-        self.assertEqual(updated_rectangles, initial_rectangles + 1)  # Сравниваем с ожидаемым результатом
+        initial_rectangles = len(self.canvas.find_all())
+        event = tk.Event()
+        self.app.draw(event)
+        updated_rectangles = len(self.canvas.find_all())
+        self.assertEqual(updated_rectangles, initial_rectangles + 1)
 
     def test_delete(self):
-        # Тест для проверки метода delete
-        event = tk.Event()  # Создаем событие
-        self.app.draw(event)  # Рисуем прямоугольник
-        initial_rectangles = len(self.canvas.find_all())  # Получаем начальное количество прямоугольников
-        event = tk.Event()  # Создаем событие (для имитации события клавиши "d")
-        self.app.delete(event)  # Вызываем метод delete
-        updated_rectangles = len(self.canvas.find_all())  # Получаем обновленное количество прямоугольников
-        self.assertEqual(updated_rectangles, initial_rectangles - 1)  # Сравниваем с ожидаемым результатом
-
-
+        event = tk.Event()
+        self.app.draw(event)
+        initial_rectangles = len(self.canvas.find_all())
+        event = tk.Event()
+        self.app.delete(event)
+        updated_rectangles = len(self.canvas.find_all())
+        self.assertEqual(updated_rectangles, initial_rectangles - 1)
 
 if __name__ == '__main__':
     unittest.main()
